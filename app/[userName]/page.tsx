4955c8b035c2ff3bca57e39d37296275b83a8fa5
Varshithvhegde <varshithvh@gmail.com>
2024-01-28 13:01:58 +0530

    Preview

--- app/[userName]/page.tsx
+++ app/[userName]/page.tsx
@@ -4,11 +4,20 @@ import React, { useRef, useState } from "react";
 import { toPng } from "html-to-image";
 // import MyCard from "./components/github-card/github-card";
 import { Button, ChakraProvider, Input, Stack } from "@chakra-ui/react";
+import {Helmet} from "react-helmet"
 import MyCard from "../components/github-card/github-card";
 
 export default function User({ params }: { params: { userName: string } }) {
     return (
         <>
+        <Helmet>
+        <title>{`Varshithvhegde's Profile`}</title>
+        <meta property="og:title" content={`${params.userName}'s Profile`} />
+        <meta property="og:description" content="Fondler" />
+        <meta property="og:image" content="https://avatars.githubusercontent.com/Varshithvhegde" />
+        <meta property="og:url" content={`URL to the user's profile page`} />
+        <meta property="og:type" content="profile" />
+      </Helmet>
         <MyCard dataUser={params.userName} authToken={process.env.NEXT_PUBLIC_GITHUB_TOKEN} dataTheme="white" />
         </>
     );
