079f1f8b2a5069b93988fbd813ffbcd11876a29d
Varshith Hegde <varshithh@kpit.com>
2024-01-28 12:43:46 +0530

    Added Custom Link Option

---
+++ app/[userName]/page.tsx
@@ -0,0 +1,17 @@
+// "use client"
+// Home.tsx
+import React, { useRef, useState } from "react";
+import { toPng } from "html-to-image";
+// import MyCard from "./components/github-card/github-card";
+import { Button, ChakraProvider, Input, Stack } from "@chakra-ui/react";
+import MyCard from "../components/github-card/github-card";
+
+export default function User({ params }: { params: { userName: string } }) {
+    return (
+        <MyCard dataUser={params.userName} authToken={process.env.NEXT_PUBLIC_GITHUB_TOKEN} dataTheme="white" />
+        // <>
+        //     {params.userName} hello
+        // </>
+    );
+  }
+  
--- app/components/my-card/page.tsx
+++ app/components/my-card/page.tsx
@@ -4,7 +4,7 @@ import MyCard from '../../components/github-card/github-card';
 const MyCardPage: React.FC = () => {
   return (
     <div style={{ width: '400px', height: '600px' }}>
-      <MyCard dataUser="varshithvhegde" dataTheme="dark" />
+      <MyCard dataUser="varshithvhegde" authToken={process.env.NEXT_PUBLIC_GITHUB_TOKEN} dataTheme="dark" />
     </div>
   );
 };
