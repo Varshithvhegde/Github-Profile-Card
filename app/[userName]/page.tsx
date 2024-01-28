321a70ec4a7962269dc0ac5c7cda8ca7ef25f48d
Varshithvhegde <varshithvh@gmail.com>
2024-01-28 13:14:56 +0530

    Head Tag Added

--- app/[userName]/page.tsx
+++ app/[userName]/page.tsx
@@ -5,16 +5,18 @@ import { toPng } from "html-to-image";
 // import MyCard from "./components/github-card/github-card";
 import { Button, ChakraProvider, Input, Stack } from "@chakra-ui/react";
 import MyCard from "../components/github-card/github-card";
-
+import { Head } from "next/document";
 export default function User({ params }: { params: { userName: string } }) {
     return (
         <>
+        <Head>
         <title>{`Varshithvhegde's Profile`}</title>
         <meta property="og:title" content={`${params.userName}'s Profile`} />
         <meta property="og:description" content="Fondler" />
         <meta property="og:image" content="https://avatars.githubusercontent.com/Varshithvhegde" />
         <meta property="og:url" content={`URL to the user's profile page`} />
         <meta property="og:type" content="profile" />
+        </Head>
         <MyCard dataUser={params.userName} authToken={process.env.NEXT_PUBLIC_GITHUB_TOKEN} dataTheme="white" />
         </>
     );
