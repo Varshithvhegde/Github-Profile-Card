dc5200ead6607328a98e34716caf05cb6cda41ae
Varshithvhegde <varshithvh@gmail.com>
2024-01-28 13:26:46 +0530

    Next Seo

--- app/[userName]/page.tsx
+++ app/[userName]/page.tsx
@@ -1,4 +1,4 @@
-// "use client"
+"use client"
 // Home.tsx
 import React, { useRef, useState } from "react";
 import { toPng } from "html-to-image";
@@ -6,17 +6,26 @@ import { toPng } from "html-to-image";
 import { Button, ChakraProvider, Input, Stack } from "@chakra-ui/react";
 import MyCard from "../components/github-card/github-card";
 import { Head } from "next/document";
+import { NextSeo } from "next-seo";
 export default function User({ params }: { params: { userName: string } }) {
     return (
         <>
-        <Head>
-        <title>{`Varshithvhegde's Profile`}</title>
-        <meta property="og:title" content={`${params.userName}'s Profile`} />
-        <meta property="og:description" content="Fondler" />
-        <meta property="og:image" content="https://avatars.githubusercontent.com/Varshithvhegde" />
-        <meta property="og:url" content={`URL to the user's profile page`} />
-        <meta property="og:type" content="profile" />
-        </Head>
+        {/* <Head> */}
+        <NextSeo
+        openGraph={{
+          type: "website",
+          title: "Varshithvhegde's Profile",
+          description: "Fondler",
+          images: [
+            {
+              url: "https://avatars.githubusercontent.com/Varshithvhegde",
+              width: 320,
+              height: 213,
+              alt: "Varshithvhegde",
+            },
+          ],
+        }}
+      />
         <MyCard dataUser={params.userName} authToken={process.env.NEXT_PUBLIC_GITHUB_TOKEN} dataTheme="white" />
         </>
     );
